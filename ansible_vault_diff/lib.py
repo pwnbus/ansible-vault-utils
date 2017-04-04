1a361c6adb8416843335f22215424ca1d9d70324
Brandon Myers <bmyers@mozilla.com>
2017-04-04 00:51:27 -0500

    Add vault_diff script with required functions
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

---
+++ ansible_vault_diff/lib.py
@@ -0,0 +1,74 @@
+import shutil
+import logging
+
+from os import path, walk
+from subprocess import Popen, PIPE, STDOUT
+from git import Repo
+
+
+logger = logging.getLogger()
+logger.level = logging.INFO
+custom_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(custom_formatter)
+logger.addHandler(stream_handler)
+
+
+def get_input(msg):
+    return raw_input(msg).rstrip()
+
+
+def run_command(cmd_str, working_dir=None):
+    process = Popen(cmd_str.split(' '), cwd=working_dir, stdout=PIPE, stderr=STDOUT)
+    stdout, stderr = process.communicate()
+    return stdout
+
+
+def remove_repo_data(repos_path):
+    if path.isdir(repos_path):
+        logger.debug('Removing tmp repo data at ' + repos_path)
+        shutil.rmtree(repos_path)
+
+
+def clone_repo(main_repo_name, remote_branch, destination):
+    remote_params = remote_branch.split(':')
+    account_name = remote_params[0]
+    branch_name = remote_params[1]
+    full_url = 'https://github.com/' + account_name + '/' + main_repo_name
+    logger.debug("Cloning {0} {1} branch into {2}".format(full_url, branch_name, destination))
+    Repo.clone_from(url=full_url, to_path=destination, branch=branch_name)
+
+
+def decrypt_file(repo_path, file_name):
+    full_file_name = path.join(repo_path, file_name)
+    logger.debug('Decrypting ' + full_file_name)
+    run_command('ansible-vault decrypt {0}'.format(full_file_name), repo_path)
+
+
+def diff_file(source_file, destination_file):
+    diff_output = run_command('diff {0} {1}'.format(source_file, destination_file))
+    return diff_output
+
+
+def diff_repo(source_repo_path, destination_repo_path):
+    results = {}
+    for subdir, dirs, files in walk(source_repo_path):
+        for file in files:
+            source_filepath = path.join(subdir, file)
+            # Skip .git files
+            if '.git/' in source_filepath:
+                continue
+            local_repo_file = source_filepath.replace(source_repo_path + '/', '')
+            destination_filepath = path.join(destination_repo_path, local_repo_file)
+            if path.isfile(destination_filepath):
+                with open(source_filepath, 'rb') as handler:
+                    source_contents = handler.read()
+                with open(destination_filepath, 'rb') as handler:
+                    destination_contents = handler.read()
+                if source_contents != destination_contents:
+                    # If the files are encrypted...
+                    if '$ANSIBLE_VAULT' in source_contents:
+                        decrypt_file(source_repo_path, local_repo_file)
+                        decrypt_file(destination_repo_path, local_repo_file)
+                        results[local_repo_file] = diff_file(source_filepath, destination_filepath)
+    return results
---
+++ setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+from ansible_vault_diff import __version__ as version
+
+
+config = {
+    'description': 'Tool used to display a diff of two ansible-vault encrypted repos',
+    'author': 'Brandon Myers',
+    'url': 'https://github.com/pwnbus/ansible-vault-diff',
+    'download_url': 'https://github.com/pwnbus/ansible-vault-diff/archive/master.zip',
+    'author_email': 'pwnbus@mozilla.com',
+    'version': version,
+    'install_requires': [
+        'GitPython==2.1.3',
+    ],
+    'packages': ['ansible_vault_diff', 'bin'],
+    'scripts': [],
+    'name': 'ansible_vault_diff'
+}
+
+setup(**config)
