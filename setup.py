4c9aaddabf1bdaae0f4b96857e71fee4e4695e9e
Brandon Myers <bmyers@mozilla.com>
2020-02-05 13:55:34 -0600

    Rename files to utils

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ except ImportError:
     from distutils.core import setup
 
 
-from ansible_vault_diff import __version__ as version
+from ansible_vault_utils import __version__ as version
 
 
 config = {
-    'description': 'Tool used to display a diff of two ansible-vault encrypted repos',
+    'description': 'A suite of tools used to interact with ansible-vault encrypted repositories',
     'author': 'Brandon Myers',
-    'url': 'https://github.com/pwnbus/ansible-vault-diff',
-    'download_url': 'https://github.com/pwnbus/ansible-vault-diff/archive/master.zip',
+    'url': 'https://github.com/pwnbus/ansible-vault-utils',
+    'download_url': 'https://github.com/pwnbus/ansible-vault-utils/archive/master.zip',
     'author_email': 'pwnbus@mozilla.com',
     'version': version,
     'install_requires': [
         'six',
         'ansible'
     ],
-    'packages': ['ansible_vault_diff', 'bin'],
+    'packages': ['ansible_vault_utils', 'bin'],
     'scripts': [],
-    'name': 'ansible_vault_diff'
+    'name': 'ansible_vault_utils'
 }
 
 setup(**config)
